8dbc4ff33899e09c6650b7b69583e9cd9b4b6daa
moritz-backes <88277138+moritz-backes@users.noreply.github.com>
2021-08-01 19:15:38 +0200

    Update multiplication_quiz.py

--- 8_Input Validation/multiplication_quiz.py
+++ 8_Input Validation/multiplication_quiz.py
@@ -1,1 +1,26 @@
-# Schreibe hier Deinen Code :-)
+import pyinputplus as pyip
+import random, time
+
+number_of_questions = 10
+correct_answers = 0
+for question_number in range(number_of_questions):
+    # pick two random numbers
+    num1 = random.randint(0, 9)
+    num2 = random.randint(0, 9)
+
+    promt = f'{question_number}: {num1} x {num2} = '
+    try:
+        # right answers are handled by allowRegexes
+        # wrong answers are handled by blockRegexes, with a custom message
+        pyip.inputStr(promt, allowRegexes=[f'^{num1 * num2}$'], blockRegexes=[('.*', 'Incorrect!')], timeout=8, limit=3)
+    except pyip.TimeoutException:
+        print('Out of time!')
+    except pyip.RetryLimitException:
+        print('Out of tries!')
+    else:
+        # this block runs if no exceptions were raised in the try block
+        print('Correct!')
+        correct_answers += 1
+    time.sleep(1) # brief pause to let the user see the result
+
+print(f'Score: {correct_answers / number_of_questions * 100}')
